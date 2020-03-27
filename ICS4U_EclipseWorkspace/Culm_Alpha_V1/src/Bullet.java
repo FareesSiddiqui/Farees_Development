acecad7981210fa2326ccd04e05f442276c22c11
FareesSiddiqui <fareesahmedsiddiqui@gmail.com>
2020-03-27 01:56:41 -0400

    bullet class

---
+++ ICS4U_EclipseWorkspace/Culm_Alpha_V1/src/Bullet.java
@@ -0,0 +1,16 @@
+
+public class Bullet {
+	
+	int x, y, speed;
+	
+	Bullet(int x1, int y1, int speed1){
+		this.x = x1;
+		this.y = y1;
+		this.speed = speed1;
+	}
+	
+	void spawnBullets() {
+		
+	}
+
+}
